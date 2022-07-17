dc6c25ad3d011c42345c515d9d49bc89f89baee5
dkaramit <dkaramit@yahoo.com>
2022-07-17 16:51:01 +0100

    using variadic templates to take multiple derivatives.

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -8,12 +8,18 @@
 namespace sadET{
 
 /*Evaluate expressions*/
-template<typename Expr, typename numType>
-constexpr auto evaluate(const Expr &expr, const map<IDType,numType> &at){return expr.evaluate(at);}
+template<typename Expr, typename numType> constexpr auto evaluate(const Expr &expr, const map<IDType,numType> &at){return expr.evaluate(at);}
+
+
 
 /*Take derivatives of expressions*/
-template<typename Expr, typename numType, IDType ID>
-constexpr auto derivative(const Expr &expr, const Variable<ID,numType> &wrt){return expr.derivative(wrt);}
+template<typename Expr, typename Var> constexpr auto derivative(const Expr &expr, const Var &wrt){return expr.derivative(wrt);}
+
+template<typename Expr, typename Var1, typename... Vars> 
+constexpr auto derivative(const Expr &expr, Var1 wrt, Vars... vars){
+    return derivative(expr.derivative(wrt),vars...);
+}
+
 
 }
 
--- test.cpp
+++ test.cpp
@@ -14,23 +14,32 @@ int main(){
     sadET::Constant<long double,-1.223> d;
     sadET::Variable<0,long double> x;
     sadET::Variable<1,double> y;
+    sadET::Variable<2,double> z;
 
-    auto z=d * (x+x*x) *y*y + c;
+    auto f= (z*d*(x + x*x)*y*y + c)*(z*d*(x + x*x)*y*y + c)*(z*d*(x + x*x)*y*y + c)*
+            (z*d*(x + x*x)*y*y + c)*(z*d*(y + x*y)*y*y + c)*(z*d*(x + x*x)*y*y + c)*
+            (z*d*(x + x*x)*y*y + c)*(z*d*(x + z*x)*y*y + c)*(z*d*(x + z*x)*y*y + c)*
+            (z*d*(x + x*x)*y*y + c)*(z*d*(x + x*x)*z*x + c)*(z*d*(x + x*z)*y*z + c);
+
 
     std::map<sadET::IDType,long double> at;
     at[0]=-2;
     at[1]=1.23;
+    at[2]=1e-7;
 
     
     cout<<evaluate(c,at)<<"\n";
     cout<<evaluate(derivative(d  ,x), at)<<"\n";
     
-    cout<<evaluate(z,at)<<"\n";
-    cout<<evaluate(derivative(z,x),at)<<"\n";
-    cout<<evaluate(derivative(z,y),at)<<"\n";
-    cout<<evaluate(derivative(derivative(z,x),x),at)<<"\n";
-    cout<<evaluate(derivative(derivative(z,y),y),at)<<"\n";
-    cout<<evaluate(derivative(derivative(z,x),y),at)<<"\n";
+    cout<<evaluate(f,at)<<"\n";
+    cout<<evaluate(derivative(f,x),at)<<"\n";
+    cout<<evaluate(derivative(f,y),at)<<"\n";
+    cout<<evaluate(derivative(f,x,x),at)<<"\n";
+    cout<<evaluate(derivative(f,y,y),at)<<"\n";
+    cout<<evaluate(derivative(f,x,y),at)<<"\n";
 
+
+    cout<<evaluate(derivative(f,x,y,z),at)<<"\n";
+    
     return 0;
 }
