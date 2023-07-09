1d05e2919b4d9e780627f6297b01884dc54b6093
Dimitrios Karamitros <dkaramit@yahoo.com>
2023-07-09 03:27:04 +0100

    added a compile time recursive sum example

--- makefile
+++ makefile
@@ -10,9 +10,12 @@ STD=-std=c++20#you need c++20 for the constants
 FLG=-I$(IncludeDir) $(OPT) $(STD) -Wall -pedantic
 
 
-all: test.run examples
+all: test.run examples sum
 
 test.run: test.cpp makefile $(IncludeHPP)
+	$(CC) -o $@ $< $(FLG) 
+
+sum.run: sum.cpp makefile $(IncludeHPP)
 	$(CC) -o $@ $< $(FLG) 
 
 examples:
---
+++ sum.cpp
@@ -0,0 +1,31 @@
+/*This is a simple program to show how you cna sum recursively without run-time loops.*/
+
+#include<iostream>
+#include<cmath>
+
+#include<sadET.hpp>
+
+
+using std::cout;
+using std::endl;
+// using sadET::evaluate;
+// using sadET::derivative;
+
+template<const size_t N>
+auto Sum(auto x){ if constexpr(N>1){return Sum<N-1>(x)+x;}else{return x;} }
+
+int main(){
+    sadET::Constant<long double,3.45> c;
+    sadET::Variable<0,double> x;
+    sadET::Variable<1,double> y;
+
+    std::map<sadET::IDType,long double> at;
+    at[0]=1.32;
+    at[1]=2.99;
+
+    cout<<evaluate( Sum<4>(x*Sum<300>(c*y)) ,at)<<endl;
+    cout<<evaluate( derivative(Sum<4>(x*Sum<3>(c*y)),x,y),at)<<endl;
+
+
+    return 0;
+}
