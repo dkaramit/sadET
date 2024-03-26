6dd35f8edd6baefc678a26893f43705b5d18ebcb
dkaramit <dkaramit@yahoo.com>
2024-03-26 22:12:53 +0200

    added a comment in experimental/new_variable.hpp

--- include/experimental/new_variable.hpp
+++ include/experimental/new_variable.hpp
@@ -1,3 +1,13 @@
+/*
+Here we copy the compile-time counter presented in https://ledas.com/post/857-how-to-hack-c-with-templates-and-friends, 
+and use it to create a new variable with unique ID without explicitely providing it. 
+
+To do this, you just do
+    auto var = sadET::new_variable<LD>();
+and you have created a new variable with ID equal to the number of 
+instances of Variable<ID,LD> created before.
+*/
+
 #include<Numbers/Variable.hpp>
 #include<misc.hpp>
 
